72e72ad0b6c532d4f0e21f8b9c758cded61f4a9e
paulgurinovich <paul.gurinovich@gmail.com>
2022-09-04 12:11:20 -0400

    Initial commit

---
+++ KeepSessionAlive/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace KeepSessionAlive
+{
+    class Program
+    {
+
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            long timeout = long.Parse(ConfigurationManager.AppSettings["timeout"]);
+            long interval = long.Parse(ConfigurationManager.AppSettings["interval"]);
+
+            KeepAliveScript(timeout, interval);
+
+        }
+
+        public static void KeepAliveScript(long timeout, long interval)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Stopwatch swC = Stopwatch.StartNew();
+
+            while (sw.ElapsedMilliseconds < timeout)
+            {
+                Process[] processes = Process.GetProcessesByName("WowClassic");
+
+                if (swC.ElapsedMilliseconds > interval)
+                {
+                    foreach (Process proc in processes)
+                    {
+                        //SetForegroundWindow(proc.MainWindowHandle);
+                        SwitchToThisWindow(proc.MainWindowHandle, true);
+
+                        SendKeys.SendWait("{UP}");
+                        SendKeys.SendWait("{DOWN}");
+                    }
+                    swC.Restart();
+                }
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern void SwitchToThisWindow(IntPtr hWnd, bool turnon);
+
+    }
+}
